f546ab981915552ece2d7c13ebe6483dadeec44d
tc <qw805880101@qq.com>
2016-11-30 17:41:19 +0800

    first commit

---
+++ app/src/main/java/com/example/fubei/activity/CityListActivity.java
@@ -0,0 +1,15 @@
+package com.example.fubei.activity;
+
+import android.os.Bundle;
+
+/**
+ * Created by tc on 2016/11/30.
+ */
+
+public class CityListActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
